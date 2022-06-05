166a7efe244d37efa35c3a49fa83a8eba89d6894
Vladisalv Goteiner <vladislavgoteiner@gmail.com>
2022-06-05 16:19:29 +0300

    another commit 5

--- src/Components/UI/Footer.js
+++ src/Components/UI/Footer.js
@@ -7,8 +7,8 @@ const Footer = () => {
         <ul className={classes.contactList}>
             <li>Phone: 054-9092278</li>
             <li>Email: va.goteiner@gmail.com</li>
-            <li>LinkedIn <a href='https://www.linkedin.com/in/vladislavgoteiner/'><img src={linkImg} alt=""></img></a></li>
-            <li>Github <a href='https://github.com/VladdyGo'><img src={linkImg} alt=""></img></a></li>
+            <li><a href='https://www.linkedin.com/in/vladislavgoteiner/'>LinkedIn <img src={linkImg} alt=""></img></a></li>
+            <li><a href='https://github.com/VladdyGo'>Github <img src={linkImg} alt=""></img></a></li>
         </ul>
     </footer>
 }
