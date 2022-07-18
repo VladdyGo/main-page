bec5e75233a9a2c9648f3eb51960f72b95dbfd09
Vladisalv Goteiner <vladislavgoteiner@gmail.com>
2022-07-18 21:18:43 +0300

    added background

--- src/Components/UI/Body.js
+++ src/Components/UI/Body.js
@@ -1,11 +1,13 @@
 import classes from "./Body.module.css";
 import animation from "../../Animations/bodyAnimation.module.css";
+import background from "../../img/CoolBackground.png";
 
 import About from "../Sections/About";
 import Projects from "../Sections/Projects";
 import Contact from "../Sections/Contact";
 import React from "react";
 import Project from "../Dialog/ProjectDialog/ProjectWindow";
+import DialogWindow from "../Dialog/BigDialogWindow/DialogWindow";
 
 const Body = (props) => {
   if (props.bodyContent === "About") {
@@ -23,16 +25,8 @@ const Body = (props) => {
   if (props.bodyContent === "VG" || props.bodyContent === "None") {
     return (
       <React.Fragment>
-        <div className={`${classes.mainBody} ${animation.runAnimation}`}>
-          <label>Finally you're here!</label>
-          <label style={{ left: "-1.875rem" }}>I was waiting for you.</label>
-          <label style={{ left: "-15.652rem" }}>
-            Feel free, click some buttons.
-          </label>
-          <label style={{ left: "-13.75rem", color: "red" }}>
-            I'm looking for a job,
-          </label>
-          <label style={{ left: "-11.875rem" }}>Contact me, Yo!</label>
+        <div className={classes.bodyStyle}>
+          <img src={background} alt=""></img>
         </div>
 
         <div className={classes.mobileBody}>
@@ -42,7 +36,7 @@ const Body = (props) => {
             title="Figure skating - machine learning"
             description="using machine learning to classify videos from figure skating olympics into their type of jump category."
             link="https://github.com/VladdyGo/Figure-Skating"
-            color='#679681'
+            color="#679681"
           ></Project>
         </div>
       </React.Fragment>
